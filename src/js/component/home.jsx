979eb1c16464e7e953995a927bbd375779889b2e
AlexPinaBr <alexpinabraojos@gmail.com>
2023-03-27 14:20:19 +0000

    Traffic Lights done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,62 @@
-import React, {useState} from "react";
-import reactDom from "react-dom";
+import React, {useState, useEffect} from "react";
+import Light from "./lights";
 
-
-
+//create your first component
 const Home = () => {
-	const [color, setColor] = useState("");
-	const [isActive, setIsActive] = useState(false);
-	const handleClick = event => {
-		setIsActive(current => !current);
-	  };
+	const [lightUp, setLightUp] = useState (0);
+	const [running, setRunning] = useState(false);
+	const lights = ["danger", "warning", "success"];
 
+	const startLights = (index) => {
+		if (running === false ) {
+		setLightUp(index);
+		setTimeout(() => {
+		const nextIndex = (index + 1) % lights.length;
+		startLights(nextIndex);
+		}, 1000);
+		}
+		}
+		
+		useEffect(() => {
+			let timeoutId = null;
+			
+			const startLights = (index) => {
+			setLightUp(index);
+			if (running) {
+			const nextIndex = (index + 1) % lights.length;
+			timeoutId = setTimeout(() => startLights(nextIndex), 1000);
+			}
+			}
+			
+			if (running) {
+			startLights(0);
+			} else {
+			clearTimeout(timeoutId);
+			}
+			
+			return () => clearTimeout(timeoutId);
+			}, [running]);
 
+			
 
 	return (
-		<div id="traffic-light">
-			<div className={color == 'red' ? "bulb-red" : "bulb"} onClick={() => setColor("red")} />
-			<div className={color == 'yellow' ? "bulb-yellow" : "bulb"} onClick={() => setColor("yellow")} />
-			<div className={color == 'green' ? "bulb-green" : "bulb"} onClick={() => setColor("green")} />
-			<button onClick={() => {
-			
-			}}>Click me to change the color</button>
-		</div>
+		<div className="text-center">
+		      <div className="trafficlight">
+                  {lights.map((x, i) =>{
+		              return <Light key={i} index={i} light={lightUp} color={x} click={setLightUp}/>
+	              })}
+              </div>
+			  <div className="d-flex justify-content-center align-items-center">
+				<button className="btn btn-primary m-2" onClick={() => {
+					lightUp == 2 ? setLightUp(0) : setLightUp(lightUp +1)
+				}}>Change TrafficLights</button>
+
+				<button className={`btn ${running ? "btn-danger" : "btn-success"} m-2`} onClick={() => setRunning(!running)}>{running ? "Stop" : "Start"}
+						</button>
+				</div>
+		</div>      
 	);
-};
+     
+			}
 
 export default Home;
